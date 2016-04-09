8c450206318a57d01ee29b999887de9cd065230e
Charles Overbeck <charles_overbeck@yahoo.com>
2016-04-09 12:29:58 -0700

    Make primary button last

--- angular-confirm.js
+++ angular-confirm.js
@@ -34,8 +34,8 @@ angular.module('angular-confirm', ['ui.bootstrap.modal'])
     template: '<div class="modal-header"><h3 class="modal-title">{{data.title}}</h3></div>' +
     '<div class="modal-body">{{data.text}}</div>' +
     '<div class="modal-footer">' +
-    '<button class="btn btn-primary" ng-click="ok()">{{data.ok}}</button>' +
     '<button class="btn btn-default" ng-click="cancel()">{{data.cancel}}</button>' +
+    '<button class="btn btn-primary" ng-click="ok()">{{data.ok}}</button>' +
     '</div>',
     controller: 'ConfirmModalController',
     defaultLabels: {
